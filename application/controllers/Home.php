308dff35283c43d547a171770f3cdeae5129b1c4
Ariefrachmanh <rachmanoah11@gmail.com>
2018-08-06 11:58:04 +0700

    master update header

--- application/controllers/Home.php
+++ application/controllers/Home.php
@@ -5,7 +5,9 @@ class Home extends CI_Controller {
 
 	public function index()
 	{
-		$this->load->view('home.php');
+		$this->load->view('komponen/header');
+		$this->load->view('kirimpesan/index');
+		$this->load->view('komponen/footer');
 	}
 
 }
--- application/views/komponen/footer.php
+++ application/views/komponen/footer.php
@@ -10,17 +10,17 @@
     </div>
 
     <!-- jQuery -->
-    <script src="asset/vendors/jquery/dist/jquery.min.js"></script>
+    <script src="<?php echo base_url(); ?>assets/vendors/jquery/dist/jquery.min.js"></script>
     <!-- Bootstrap -->
-    <script src="asset/vendors/bootstrap/dist/js/bootstrap.min.js"></script>
+    <script src="<?php echo base_url(); ?>assets/vendors/bootstrap/dist/js/bootstrap.min.js"></script>
     <!-- FastClick -->
-    <script src="asset/vendors/fastclick/lib/fastclick.js"></script>
+    <script src="<?php echo base_url(); ?>assets/vendors/fastclick/lib/fastclick.js"></script>
     <!-- NProgress -->
-    <script src="asset/vendors/nprogress/nprogress.js"></script>
+    <script src="<?php echo base_url(); ?>assets/vendors/nprogress/nprogress.js"></script>
     <!-- jQuery custom content scroller -->
-    <script src="asset/vendors/malihu-custom-scrollbar-plugin/jquery.mCustomScrollbar.concat.min.js"></script>
+    <script src="<?php echo base_url(); ?>assets/vendors/malihu-custom-scrollbar-plugin/jquery.mCustomScrollbar.concat.min.js"></script>
 
     <!-- Custom Theme Scripts -->
-    <script src="asset/build/js/custom.min.js"></script>
+    <script src="<?php echo base_url(); ?>assets/build/js/custom.min.js"></script>
   </body>
 </html>
